0f3a151d46e8f8b841c4b2684898d42a6f36e666
ZachBockheim <zach.bockheim@gmail.com>
2024-10-07 23:24:09 -0700

    StackComputer code I get the right output here hope I do on github as well"

--- StackComputer.java
+++ StackComputer.java
@@ -1,31 +1,60 @@
+import java.util.*;
+
 /**
  * Create a stack computer
  */
 public class StackComputer {
+    public int SizeOfArray;
+    public static int[] array;
+    public int add;
+    public int addtemp;
+    public int pop;
+    public int peek;
+    public int counter = 0;
+    public int maxsize;
+    public int multemp;
+    public int mul;
+    public int divtemp;
+    public int div;
+    public int sub;
+    public int subtemp;
+
     /**
      * Create a stack computer with a given maximum stack size
      */
     public StackComputer(int maxsize) {
+        this.maxsize = maxsize;
+        array = new int[maxsize];
+
     }
 
     /**
      * Return the array of ints that represents the stack
      */
     public int[] getStack() {
-        return null;
+        return array;
     }
 
     /**
      * Return the number of elements in the stack
      */
     public int size() {
-        return 0;
+
+        return counter;
     }
 
     /**
-     * Push a value on the stack. Throw exception if the stack is full.
+     * Push a value on the stack. If stack full
+     * throw a StackException
      */
     public void push(int value) throws StackException {
+        if (counter == maxsize) {
+            throw new StackException();
+        } else {
+            array[counter] = value;
+            counter++;
+        }
+
     }
 
     /**
@@ -33,7 +62,15 @@ public class StackComputer {
      * throw a StackException
      */
     public int pop() throws StackException {
-        return 0;
+        if (counter == maxsize) {
+            throw new StackException();
+        } else {
+            pop = array[counter - 1];
+            array[counter - 1] = 0;
+            counter--;
+        }
+        return pop;
+
     }
 
     /**
@@ -41,7 +78,12 @@ public class StackComputer {
      * empty
      */
     public int peek() throws StackException {
-        return 0;
+        if (counter == maxsize) {
+            throw new StackException();
+        } else {
+            peek = array[counter - 1];
+        }
+        return peek;
     }
 
     /**
@@ -49,6 +91,19 @@ public class StackComputer {
      * Throw a StackException if the stack is empty
      */
     public void add() throws StackException {
+        if (counter == maxsize) {
+            throw new StackException();
+        } else {
+            addtemp = array[counter - 2];
+            array[counter - 2] = 0;
+            add = array[counter - 1];
+            array[counter - 1] = 0;
+            int addValue = addtemp + add;
+            counter = counter - 2;
+            array[counter] = addValue;
+            counter++;
+        }
+
     }
 
     /**
@@ -58,6 +113,18 @@ public class StackComputer {
      * Throw a StackException if the stack is empty.
      */
     public void sub() throws StackException {
+        if (counter == maxsize) {
+            throw new StackException();
+        } else {
+            subtemp = array[counter - 2];
+            array[counter - 2] = 0;
+            sub = array[counter - 1];
+            array[counter - 1] = 0;
+            int subValue = subtemp - sub;
+            counter = counter - 2;
+            array[counter] = subValue;
+            counter++;
+        }
     }
 
     /**
@@ -66,6 +133,18 @@ public class StackComputer {
      * Throw a StackException if the stack is empty
      */
     public void mul() throws StackException {
+        if (counter == maxsize) {
+            throw new StackException();
+        } else {
+            multemp = array[counter - 2];
+            array[counter - 2] = 0;
+            mul = array[counter - 1];
+            array[counter - 1] = 0;
+            int mulValue = multemp * mul;
+            counter = counter - 2;
+            array[counter] = mulValue;
+            counter++;
+        }
     }
 
     /**
@@ -75,6 +154,18 @@ public class StackComputer {
      * Throw a StackException if the stack is empty
      */
     public void div() throws StackException {
+        if (counter == maxsize) {
+            throw new StackException();
+        } else {
+            divtemp = array[counter - 2];
+            array[counter - 2] = 0;
+            div = array[counter - 1];
+            array[counter - 1] = 0;
+            int divValue = divtemp / div;
+            counter = counter - 2;
+            array[counter] = divValue;
+            counter++;
+        }
     }
 
     public static void main(String[] args) throws StackException {
@@ -82,13 +173,13 @@ public class StackComputer {
         comp.push(5);
         comp.push(10);
         comp.push(3);
-        
-        System.out.println(comp.size());     // should be 3
+
+        System.out.println(comp.size()); // should be 3
 
         comp.mul();
-        System.out.println(comp.peek());     // should be 30
+        System.out.println(comp.peek()); // should be 30
 
         comp.sub();
-        System.out.println(comp.pop());      // should be -25
+        System.out.println(comp.pop()); // should be -25
     }
 }
